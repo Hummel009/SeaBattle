c5dc0a7cc4fc80bb6b38e3bee8f055c9c7912d5d
Hummel009 <hummel.turbamentum@gmail.com>
2024-07-20 15:46:32 +0300

    3

--- android/src/main/java/com/github/hummel/sb/AndroidLauncher.java
+++ android/src/main/java/com/github/hummel/sb/AndroidLauncher.java
@@ -1,13 +1,12 @@
 package com.github.hummel.sb;
 
 import android.os.Bundle;
-
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(SeaBattle.INSTANCE, config);
