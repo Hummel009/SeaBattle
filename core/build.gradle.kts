ba65d41602aaa3418d43917d5f1c36eb53bd7331
Hummel009 <hummel.turbamentum@gmail.com>
2024-07-20 15:42:34 +0300

    1

---
+++ core/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+	id("org.jetbrains.kotlin.jvm")
+}
+
+dependencies {
+	api("com.badlogicgames.gdx:gdx-freetype:1.12.1")
+}
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(8)
+	}
+}
